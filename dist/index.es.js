d12dd453c91fb552fc30268f00eb517040b7c58f
Kazachenko Viacheslav <kreeon@ya.ru>
2021-05-18 00:31:43 +0300

    update lib

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,109 +1,110 @@
 import { createMachine, assign, interpret } from 'xstate';
 
-function validator(context, event) {
-    const { value } = event;
-    const { rules } = context;
-    const rulesLength = rules?.length || 0;
-    return new Promise((resolve, reject) => {
-        for (let i = 0; i < rulesLength; i++) {
-            const checkResult = rules[i](value);
-            if (checkResult !== true) {
-                reject(new Error(checkResult));
-                return;
-            }
-        }
-
-        resolve();
-    });
-}
-
 const fieldValidationMachine = createMachine(
-    {
-        id: "field-validation",
-        initial: "idle",
-        states: {
-            idle: {
-                on: {
-                    VALIDATE: { target: "validating" },
-                },
-            },
-            validating: {
-                invoke: {
-                    id: "sync-validation",
-                    src: validator,
-                    onDone: {
-                        target: "valid",
-                        actions: "clearError",
-                    },
-                    onError: {
-                        target: "invalid",
-                        actions: "setError",
-                    },
-                },
-            },
-            valid: {
-                on: {
-                    VALIDATE: {
-                        target: "validating",
-                    },
-                    UPDATE_RULES: {
-                        actions: "updateRules"
-                    }
-                },
-            },
-            invalid: {
-                on: {
-                    VALIDATE: {
-                        target: "validating",
-                    },
-                    UPDATE_RULES: {
-                        actions: "updateRules"
-                    }
-                },
-            },
+  {
+    id: "field-validation",
+    initial: "idle",
+    states: {
+      idle: {
+        on: {
+          VALIDATE: { target: "validating" },
         },
+      },
+      validating: {
+        invoke: {
+          id: "sync-validation",
+          src: validator,
+          onDone: {
+            target: "valid",
+            actions: "clearError",
+          },
+          onError: {
+            target: "invalid",
+            actions: "setError",
+          },
+        },
+      },
+      valid: {
+        on: {
+          VALIDATE: {
+            target: "validating",
+          },
+          UPDATE_RULES: {
+            actions: "updateRules",
+          },
+        },
+      },
+      invalid: {
+        on: {
+          VALIDATE: {
+            target: "validating",
+          },
+          UPDATE_RULES: {
+            actions: "updateRules",
+          },
+        },
+      },
     },
-    {
-        actions: {
-            clearError: assign({
-                error: null,
-            }),
-            setError: assign({
-                error: (context, event) => event.data.message,
-            }),
-            updateRules: assign({
-                rules: (context, event) => event.rules,
-            })
-        },
-    }
+  },
+  {
+    actions: {
+      clearError: assign({
+        error: null,
+      }),
+      setError: assign({
+        error: (context, event) => event.data.message,
+      }),
+      updateRules: assign({
+        rules: (context, event) => event.rules,
+      }),
+    },
+  }
 );
 
-
-const createValidationService = ({rules = []}) => {
-    const machine = fieldValidationMachine.withContext({
-        error: null,
-        rules
-    });
-
-    const service = interpret(machine);
-
-    service.start();
-
-    const validate = (value) => service.send({ type: "VALIDATE", value });
-    const updateRules = (rules) => service.send({ type: "UPDATE_RULES", rules });
-    
-    function register(field) {
-        // could be different validation types
-        field.addEventListener("input", (e) => {
-            validate(e.target.value);
-        });
+function validator(context, event) {
+  const { value } = event;
+  const { rules } = context;
+  const rulesLength = rules?.length || 0;
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < rulesLength; i++) {
+      const checkResult = rules[i](value);
+      if (checkResult !== true) {
+        reject(new Error(checkResult));
+        return;
+      }
     }
 
-    return {
-        service,
-        register,
-        updateRules,
-    };
+    resolve();
+  });
+}
+
+const createValidationService = ({ rules = [] }) => {
+  const machine = fieldValidationMachine.withContext({
+    error: null,
+    rules,
+  });
+
+  const service = interpret(machine);
+
+  service.start();
+
+  const validate = (value) => service.send({ type: "VALIDATE", value });
+  const updateRules = (rules) => service.send({ type: "UPDATE_RULES", rules });
+
+  function register(field) {
+    // could be different validation types
+    field.addEventListener("input", (e) => {
+      validate(e.target.value);
+    });
+  }
+
+  return {
+    service,
+    register,
+    updateRules,
+    validate,
+    machine,
+  };
 };
 
 export { createValidationService };
