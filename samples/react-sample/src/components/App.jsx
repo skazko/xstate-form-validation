e5e38a729a4321da7d9ae4f3fedc985f96e0eee0
Kazachenko Viacheslav <kreeon@ya.ru>
2021-05-31 13:33:34 +0300

    update sample

--- samples/react-sample/src/components/App.jsx
+++ samples/react-sample/src/components/App.jsx
@@ -1,25 +1,22 @@
-import { useFormValidateMachine } from "../hooks/useFormValidate";
+import { useFormMachine } from "../hooks/useFormValidate";
 
 function App() {
-  const { register, send, form } = useFormValidateMachine({
-    onSubmit: async (data) => {
-      console.log(data)
+  const { register, submit, form } = useFormMachine({
+    onSubmit: (data) => {
+      console.log(data);
       return Promise.resolve();
-    }
+    },
   });
-  const { hasError, errors } = form;
+  const { hasError, errors, fields } = form;
+  const { name, password } = fields;
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    send("submit");
-  };
   return (
-    <form style={{ margin: "0 auto", maxWidth: "640px", paddingTop: "50px" }} onSubmit={onSubmit}>
+    <form style={{ margin: "0 auto", maxWidth: "640px", paddingTop: "50px" }} onSubmit={submit}>
       <div className="formField">
         <div className="fieldContainer">
           <input
             {...register({
-              name: 'name',
+              name: "name",
               rules: [
                 (v) => !!v || "Введите имя",
                 (v) => v.trim().length >= 3 || "Имя должно быть как минимум из 3 символов",
@@ -28,7 +25,7 @@ function App() {
             className="field"
             type="text"
           />
-          {errors.name !== null && <span style={{ color: "red" }}>{errors.name}</span>}
+          {name.hasError && <span style={{ color: "red" }}>{errors.name}</span>}
         </div>
       </div>
       <div className="formField">
@@ -44,7 +41,7 @@ function App() {
             className="field"
             type="password"
           />
-          {errors.password !== null && <span style={{ color: "red" }}>{errors.password}</span>}
+          {password.hasError && <span style={{ color: "red" }}>{errors.password}</span>}
         </div>
       </div>
 
--- samples/react-sample/src/hooks/useFormValidate.js
+++ samples/react-sample/src/hooks/useFormValidate.js
@@ -1,46 +1,21 @@
 import { useMachine } from "@xstate/react";
-import { formMachine } from "xstate-form-validation";
+import { formMachine, utils } from "xstate-form-validation";
+const { createRegister, createFormData, createSubmit, createSubmitService } = utils;
 
-function useRegisterField(state, send) {
-  const { fields } = state.context;
+export function useFormMachine({ onSubmit = () => Promise.resolve() } = {}) {
+  const [state, send] = useFormMachineWithSubmitService(formMachine, onSubmit);
+  const register = createRegister(state, send);
+  const submit = createSubmit(send);
+  const form = createFormData(state);
 
-  return ({ rules = [], name, defaultValue = "" } = {}) => {
-    if (!(name in fields)) {
-      console.log("register: ", name);
-      send({
-        type: "FIELD.ADD",
-        field: {
-          name,
-          value: defaultValue,
-          rules,
-        },
-      });
-    }
-
-    return {
-      name,
-      onChange: (e) => {
-        fields[name].ref.send({ type: "input", value: e.target.value });
-      },
-    };
-  };
+  return { register, submit, form };
 }
 
-export function useFormValidateMachine({ onSubmit = () => {} } = {}) {
-  const submitService = (context) => {
-    const data = Object.fromEntries(Object.entries(context.fields).map(([key, value]) => [key, value.value]));
-    return onSubmit(data);
-  };
-  const [state, send] = useMachine(formMachine.withConfig({ services: { submitService } }), { devTools: true });
-  const register = useRegisterField(state, send);
+function useFormMachineWithSubmitService(machine, submitCb) {
+  const [state, send, service] = useMachine(
+    machine.withConfig({ services: { submitService: createSubmitService(submitCb) } }),
+    { devTools: true }
+  );
 
-  const form = {
-    hasError: state.matches("error"),
-    errors: {},
-  };
-  for (let key in state.context.fields) {
-    form.errors[key] = state.context.fields[key].error;
-  }
-
-  return { register, send, form };
+  return [state, send, service];
 }
